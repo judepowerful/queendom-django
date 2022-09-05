5cb05af65a5f4bf8a582b1bb39c8c3b02c1fe531
之之 <99943354@qq.com>
2022-09-05 00:15:24 -0400

    test2

---
+++ staticfiles/js/user-profile.5142662b9997.js
@@ -0,0 +1,8 @@
+//-----Friends Profile Start----
+
+// Profile Friend Menu(Friend按钮)
+var friendmenu = document.querySelector(".profile-friend-menu");
+
+function friendMenuToggle(){
+  friendmenu.classList.toggle("profile-friend-menu-height");
+}
