39c11451272e0b83df3e5ea5b58b670a0df3ad49
之之 <99943354@qq.com>
2022-10-02 02:34:45 -0400

    fix

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,4 @@
 from dataclasses import field
-from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
