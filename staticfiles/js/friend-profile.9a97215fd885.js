4d794859bad0ab4c609776acc80f54080ba5bc2b
Peng Yu <99943354@qq.com>
2022-12-03 19:41:15 -0500

    remove heroku and static

--- staticfiles/js/friend-profile.9a97215fd885.js
+++
@@ -1,8 +0,0 @@
-//-----Friends Profile Start----
-
-// Blocked Menu(三个点按钮)
-var blockedmenu = document.querySelector(".blocked-menu");
-
-function blockedMenuToggle(){
-  blockedmenu.classList.toggle("blocked-menu-height");
-}
