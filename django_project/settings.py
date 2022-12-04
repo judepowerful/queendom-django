6e65c8158ee8a0e78526ea9fedeeb3f2a7acf56e
Peng Yu <99943354@qq.com>
2022-12-03 19:38:06 -0500

    remove heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'frontend/build/static')
 # ]
